2dab7ace52e08428e28dec8d2c6c45b85f908a8f
Minigonga <minigonga@gmail.com>
2023-11-17 19:57:12 +0000

    Classe wall e background no quadro de jogo

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,6 @@
+import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
@@ -6,19 +8,23 @@ import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game {
     private Player p1;
     private Player p2;
-    private int heigth;
+    private int height;
     private int width;
+    private List<Wall> walls;
     Screen screen;
     Game() throws IOException{
         p1 = new Player(1,1,"#123456");
         p2 = new Player(4,3,"#987654");
-        heigth = 80;
+        height = 80;
         width = 80;
-        TerminalSize terminalSize = new TerminalSize(width, heigth);
+        this.walls = createWalls();
+        TerminalSize terminalSize = new TerminalSize(width, height);
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
         Terminal terminal = terminalFactory.createTerminal();
         screen = new TerminalScreen(terminal);
@@ -27,10 +33,29 @@ public class Game {
         screen.doResizeIfNecessary();
     }
 
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+        for (int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height - 1));
+        }
+        for (int r = 1; r < height - 1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width - 1, r));
+        }
+        return walls;
+    }
+
     public void draw() throws IOException{
         screen.clear();
-        p1.draw(screen.newTextGraphics());
-        p2.draw(screen.newTextGraphics());
+        TextGraphics graphics = screen.newTextGraphics();
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#FFFFFF"));
+        graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
+        p1.draw(graphics);
+        p2.draw(graphics);
+        for (Wall wall: walls){
+            wall.draw(graphics);
+        }
         screen.refresh();
     }
 
---
+++ src/main/java/Wall.java
@@ -0,0 +1,16 @@
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+public class Wall extends Objects{
+    Wall(int x, int y){
+        super (x,y);
+    }
+    public void draw(TextGraphics graphics){
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#0000FF"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(getPos().getX(), getPos().getY()), " ");
+    }
+}
