ab39babdb29123e17e8d773587f3dd8971301584
Manuel Mo <manuelmo.steam@gmail.com>
2023-11-25 23:37:52 +0000

    Teste para o boost adicionado. mockit usado :)

--- src/test/java/org/l07g09/model/game/element/PlayerTest.java
+++ src/test/java/org/l07g09/model/game/element/PlayerTest.java
@@ -1,16 +1,24 @@
 package org.l07g09.model.game.element;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.l07g09.model.game.element.Player;
 import org.l07g09.model.Position;
+import org.mockito.Mockito;
 
 import static org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlayerTest {
-    private final Player player = new Player(0,0,"#FFFFFF", "1");
+    private Player player;
+    @BeforeEach
+    public void setUp() {
+        player = new Player(0,0,"#FFFFFF", "1");
+    }
     @Test
     public void Player() {
         Player p = new Player(0,0,"#FFFFFF", "1");
@@ -27,11 +35,27 @@ public class PlayerTest {
         assertEquals(0, player.getPos().getY());
     }
     @Test
+    public void boost() {
+        player.setDirection(0);
+        Player mockedPlayer = Mockito.spy(player);
+        Mockito.doNothing().when(mockedPlayer).move();
+        assertEquals(0, mockedPlayer.getBoostCount());
+        mockedPlayer.boost();
+        Mockito.verify(mockedPlayer, Mockito.times(7)).move();
+        assertEquals(1, mockedPlayer.getBoostCount());
+        mockedPlayer.boost();
+        mockedPlayer.boost();
+        Mockito.verify(mockedPlayer, Mockito.times(21)).move();
+        assertEquals(3, mockedPlayer.getBoostCount());
+        mockedPlayer.boost(); //O player não vai conseguir usar mais boost.
+        Mockito.verify(mockedPlayer, Mockito.times(21)).move();
+        assertEquals(3, mockedPlayer.getBoostCount());
+    }
+    @Test
     public void move() {
-        Position p = new Position(1,0);
         player.setDirection(0);
         player.move();
-        assertTrue(player.getPos().equals(p));
+        assertTrue(player.getPos().equals(new Position(1,0)));
         assertEquals(0, player.getPos().getY());
     }
     @Test
