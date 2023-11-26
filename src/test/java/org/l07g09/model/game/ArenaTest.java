a55eb7c6c482b9d0e9b73be7edf38c8d3e31eee5
Manuel Mo <manuelmo.steam@gmail.com>
2023-11-26 16:27:04 +0000

    Ainda não dá para testar a arena.

--- src/test/java/org/l07g09/model/game/ArenaTest.java
+++ src/test/java/org/l07g09/model/game/ArenaTest.java
@@ -1,4 +1,21 @@
 package org.l07g09.model.game;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class ArenaTest {
+    Arena arena;
+    @BeforeEach
+    public void setUp() throws IOException, URISyntaxException, FontFormatException {
+        arena = new Arena();
+    }
+    @Test
+    public void collision() {
+        //Por enquanto, não conseguimos testar os métodos da classe Arena, uma vez que esta não está na sua melhor forma para ser testada, pois o construtor cria já os jogadores.
+    }
+
 }
