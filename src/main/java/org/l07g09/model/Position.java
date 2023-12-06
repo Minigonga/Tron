21c96855f103dda427d8b3392c4d39484197e30e
Manuel Mo <manuelmo.steam@gmail.com>
2023-12-06 21:14:51 +0000

    Adições de novas funções gets

--- src/main/java/org/l07g09/model/Position.java
+++ src/main/java/org/l07g09/model/Position.java
@@ -24,4 +24,20 @@ public class Position {
     public void setY(int y) {
         this.y = y;
     }
+
+    public Position getLeft() {
+        return new Position(x - 1, y);
+    }
+
+    public Position getRight() {
+        return new Position(x + 1, y);
+    }
+
+    public Position getUp() {
+        return new Position(x, y - 1);
+    }
+
+    public Position getDown() {
+        return new Position(x, y + 1);
+    }
 }
