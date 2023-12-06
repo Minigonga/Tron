3a16e8c72d3edbec35085b8798a470164ee6d089
Manuel Mo <manuelmo.steam@gmail.com>
2023-12-06 21:41:06 +0000

    Adição de funções para controlar a arena.

--- src/main/java/org/l07g09/controller/game/ArenaController.java
+++ src/main/java/org/l07g09/controller/game/ArenaController.java
@@ -1,4 +1,23 @@
 package org.l07g09.controller.game;
 
-public class ArenaController {
+import org.l07g09.Game;
+import org.l07g09.gui.GUI;
+import org.l07g09.model.game.Arena;
+
+
+public class ArenaController extends GameController {
+    private final PlayersController playersController;
+
+    public ArenaController(Arena arena) {
+        super(arena);
+        this.playersController = new PlayersController(arena);
+    }
+    @Override
+    public void step(Game game, GUI.Action action, long time) {
+        if (action == GUI.Action.exit) {
+            game.setState(null);
+        } else {
+            playersController.step(game, action, time);
+        }
+    }
 }
