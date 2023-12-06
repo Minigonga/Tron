31849c7044088a47c76d2fdcffa7324b525ad3f0
Minigonga <minigonga@gmail.com>
2023-12-06 18:24:11 +0000

    Criação da classe state e atualização da classe game

--- src/main/java/org/l07g09/Game.java
+++ src/main/java/org/l07g09/Game.java
@@ -1,14 +1,38 @@
 package org.l07g09;
 
-import org.l07g09.model.game.Arena;
+import org.l07g09.gui.LanternaGUI;
+import org.l07g09.states.State;
 
 import java.awt.*;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class Game {
-    public static void main(String[] args) throws IOException, URISyntaxException, FontFormatException {
-        Arena arena = new Arena();
-        arena.run();
+    private final LanternaGUI gui;
+    private State state;
+
+    public Game() throws FontFormatException, IOException, URISyntaxException {
+        this.gui = new LanternaGUI(20, 20);
+        //this.state = new GameState(new Menu());
+    }
+
+    public static void main(String[] args) throws IOException, FontFormatException, URISyntaxException, InterruptedException {
+        new Game().start();
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    private void start() throws InterruptedException, IOException {
+        int FPS = 20;
+        int frameTime = 1000 / FPS;
+        while (this.state != null) {
+            long startTime = System.currentTimeMillis();
+            //state.step(this, gui, startTime);
+            long sleepTime = frameTime - System.currentTimeMillis() - startTime;
+            if (sleepTime > 0) Thread.sleep(sleepTime);
+        }
+        gui.close();
     }
 }
--- src/main/java/org/l07g09/states/State.java
+++ src/main/java/org/l07g09/states/State.java
@@ -1,4 +1,21 @@
 package org.l07g09.states;
 
-public class State {
+public abstract class State<T> {
+    private final T model;/*
+    private final Controller<T> controller;
+    private final Viewer<T> viewer;*/
+
+    public State(T model) {
+        this.model = model;/*
+        this.viewer = getViewer();
+        this.controller = getController();*/
+    }
+    /*
+    protected abstract Viewer<T> getViewer();
+
+    protected abstract Controller<T> getController();*/
+
+    public T getModel() {
+        return model;
+    }
 }
