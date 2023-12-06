f7a0f93f6d089f9c843c87e4f497ebead9242407
Minigonga <minigonga@gmail.com>
2023-12-06 18:51:43 +0000

    Classes do viewer implementadas e atualizações no state para conectar com o viewer.

--- src/main/java/org/l07g09/viewer/game/ElementViewer.java
+++ src/main/java/org/l07g09/viewer/game/ElementViewer.java
@@ -1,4 +1,8 @@
 package org.l07g09.viewer.game;
 
-public interface ElementViewer {
+import org.l07g09.gui.GUI;
+import org.l07g09.model.game.element.Element;
+
+public interface ElementViewer<T extends Element> {
+    void draw(T element, GUI gui);
 }
