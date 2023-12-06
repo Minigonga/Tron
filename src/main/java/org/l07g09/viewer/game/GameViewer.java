fdbbd1a753f9c4b3459ab882039d042d75c48838
Minigonga <minigonga@gmail.com>
2023-12-06 19:26:07 +0000

    Alteração dos Wall e Trail para Block.

--- src/main/java/org/l07g09/viewer/game/GameViewer.java
+++ src/main/java/org/l07g09/viewer/game/GameViewer.java
@@ -3,6 +3,7 @@ package org.l07g09.viewer.game;
 import org.l07g09.gui.GUI;
 import org.l07g09.model.game.Arena;
 import org.l07g09.model.game.element.Element;
+import org.l07g09.model.game.element.Player;
 import org.l07g09.viewer.Viewer;
 
 import java.util.List;
@@ -14,9 +15,13 @@ public class GameViewer extends Viewer<Arena> {
 
     @Override
     public void drawElements(GUI gui) {
-        drawElements(gui, getModel().getWalls(), new WallViewer());
-        drawElement(gui, getModel().getPlayer1(), new PlayerViewer());
-        drawElement(gui, getModel().getPlayer2(), new PlayerViewer());
+        drawElements(gui, getModel().getWalls(), new BlockViewer());
+        Player player1 = getModel().getPlayer1();
+        Player player2 = getModel().getPlayer2();
+        drawElement(gui, player1, new PlayerViewer());
+        drawElement(gui, player2, new PlayerViewer());
+        drawElements(gui, player1.getTrails(), new BlockViewer());
+        drawElements(gui, player2.getTrails(), new BlockViewer());
     }
 
     private <T extends Element> void drawElements(GUI gui, List<T> elements, ElementViewer<T> viewer) {
