af227d1df864bf5e871814e9f589365d4c1403ca
Manuel Mo <manuelmo.steam@gmail.com>
2023-12-07 10:26:55 +0000

    adição do clear e do refresh no draw.

--- src/main/java/org/l07g09/viewer/Viewer.java
+++ src/main/java/org/l07g09/viewer/Viewer.java
@@ -16,7 +16,9 @@ public abstract class Viewer<T> {
     }
 
     public void draw(GUI gui) throws IOException {
+        gui.clear();
         drawElements(gui);
+        gui.refresh();
     }
 
     protected abstract void drawElements(GUI gui);
