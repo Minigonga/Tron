20ee28042a929e85996ae8c832f270491ffcd3e8
Minigonga <minigonga@gmail.com>
2023-12-08 23:02:37 +0000

    Dar set ao finish que acrescentamos ao game para saber se termina o programa ou abre o jogo

--- src/main/java/org/l07g09/controller/menu/MenuController.java
+++ src/main/java/org/l07g09/controller/menu/MenuController.java
@@ -3,11 +3,14 @@ package org.l07g09.controller.menu;
 import org.l07g09.Game;
 import org.l07g09.controller.Controller;
 import org.l07g09.gui.GUI;
+import org.l07g09.gui.LanternaGUI;
 import org.l07g09.model.game.arena.ArenaBuilder;
 import org.l07g09.model.menu.Menu;
 import org.l07g09.states.GameState;
 
+import java.awt.*;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class MenuController extends Controller<Menu> {
     public MenuController(Menu menu) {
@@ -15,7 +18,7 @@ public class MenuController extends Controller<Menu> {
     }
 
     @Override
-    public void step(Game game, GUI.Action action, long time) {
+    public void step(Game game, GUI.Action action) throws IOException, URISyntaxException, FontFormatException {
         switch (action) {
             case up2:
                 getModel().previousEntry();
@@ -24,8 +27,13 @@ public class MenuController extends Controller<Menu> {
                 getModel().nextEntry();
                 break;
             case select:
-                if (getModel().isExit()) game.setState(null);
-                if (getModel().isStart()) game.setState(new GameState(new ArenaBuilder().createArena()));
+                if (getModel().isExit()) {
+                    game.setState(null);
+                }
+                if (getModel().isStart()) {
+                    game.setState(null);
+                    game.setFinish(1);
+                }
         }
     }
 }
