9c2bc66789e4adac8c0cff0a1c2bb6f829680454
Minigonga <minigonga@gmail.com>
2023-12-08 23:08:43 +0000

    Separar o gui em dois (guiArena e guiMenu) e criar o finish para saber quando é que o programa termina ou começa o jogo.

--- src/main/java/org/l07g09/Game.java
+++ src/main/java/org/l07g09/Game.java
@@ -13,35 +13,50 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class Game {
-    private final LanternaGUI gui;
+    private LanternaGUI guiArena;
+    private LanternaGUI guiMenu;
+    private int finish;
     private State state;
 
     public Game() throws FontFormatException, IOException, URISyntaxException {
-        this.gui = new LanternaGUI(280, 190);
+        this.guiMenu = new LanternaGUI(30,25, 30);
         this.state = new MenuState(new Menu());
+        this.finish=0;
     }
 
     public static void main(String[] args) throws IOException, FontFormatException, URISyntaxException, InterruptedException {
-        new Game().start();
+        new Game().startMenu();
     }
-    private void start() throws InterruptedException, IOException {
-        int FPS = 20;
-        long frameTime = 1000 / FPS;
+    private void startMenu() throws IOException, URISyntaxException, FontFormatException {
         int i = 0;
         while (this.state != null) {
             i++;
-            if(i%20000000==0) {
-                long startTime = System.currentTimeMillis();
-                state.step(this, gui, startTime);
-                long sleepTime = frameTime - System.currentTimeMillis() - startTime;
-                if (sleepTime > 0) Thread.sleep(sleepTime);
-            }
+            if(i%2000000==0) state.step(this, guiMenu);
         }
-        gui.close();
+        guiMenu.close();
+        if (finish==1) {
+            this.state = new GameState(new ArenaBuilder().createArena());
+            this.guiArena = new LanternaGUI(280, 190, 4);
+            startArena();
+        }
+    }
+    public void startArena() throws IOException, URISyntaxException, FontFormatException {
+        finish=0;
+        setState(new GameState(new ArenaBuilder().createArena()));
+        while (this.state != null) {
+            state.step(this, guiArena);
+        }
+        guiArena.close();
+        guiMenu = new LanternaGUI(30,25,30);
+        this.state = new MenuState(new Menu());
+        startMenu();
     }
 
     public void setState(State state) {
         this.state = state;
     }
 
+    public void setFinish(int finish) {
+        this.finish = finish;
+    }
 }
