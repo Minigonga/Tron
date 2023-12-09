8c3e87915ac4206ac5a3c694ddd5fbcff0b3a3d2
Manuel Mo <manuelmo.steam@gmail.com>
2023-12-09 14:23:29 +0000

    Adição o oneBoost.

--- src/main/java/org/l07g09/model/game/scoreboard/ScoreBoard.java
+++ src/main/java/org/l07g09/model/game/scoreboard/ScoreBoard.java
@@ -25,11 +25,11 @@ public class ScoreBoard {
         else if (score2 == 3) makeThreeScore(2);
     }
     private void makeBoost(int boost1, int boost2) {
-        if (boost1 == 0) makeZeroBoost(1);
+        if (boost1 == 0) makeOneBoost(1);
         /*else if (boost1 == 1) makeOneBoost(1);
         else if (boost1 == 2) makeTwoBoost(1);
         else if (boost1 == 3) makeTwoBoost(1);*/
-        if (boost2 == 0) makeZeroBoost(2);
+        if (boost2 == 0) makeTwoBoost(2);
         /*else if (boost2 == 1) makeOneBoost(2);
         else if (boost2 == 2) makeTwoBoost(2);
         else if (boost2 == 3) makeTwoBoost(2);*/
@@ -163,7 +163,11 @@ public class ScoreBoard {
         List<Block> local = new ArrayList<>();
         int y;
         if (who == 1) y = 53; else y=119;
-
+        for (int c = 0; c < 5; c++) {
+            if (c <= 1 || c >= 3) for (int r = 0; r < 2; r++) local.add(new Block(c+231, r + y+6, color));
+            if (c == 1) for (int r = 0; r < 1; r++) local.add(new Block(c+231, r + y+2, color));
+            if (c ==2) for (int r = 0; r <8; r++) local.add(new Block(c+231, r + y, color));
+        }
         if (who == 1) boost1View = local;
         else boost2View = local;
     }
