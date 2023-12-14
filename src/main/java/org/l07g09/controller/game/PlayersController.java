773f6a52b1f7e809882ee8169a4af17d2538ee9b
Minigonga <minigonga@gmail.com>
2023-12-14 12:28:21 +0000

    Uso do cooldown no player controller

--- src/main/java/org/l07g09/controller/game/PlayersController.java
+++ src/main/java/org/l07g09/controller/game/PlayersController.java
@@ -38,6 +38,10 @@ public class PlayersController extends GameController {
     public void step(Game game, GUI.Action action) {
         Player p1 = getModel().getPlayer1();
         Player p2 = getModel().getPlayer2();
+        int coolDown1= getModel().getJumpCd1();
+        int coolDown2= getModel().getJumpCd2();
+        if (coolDown1!=0){coolDown1--;}
+        if (coolDown2!=0){coolDown2--;}
         int mov1=0;
         int mov2=0;
         int dir1 = p1.getDirection(), dir2 = p2.getDirection();
@@ -45,28 +49,36 @@ public class PlayersController extends GameController {
         if (action == GUI.Action.right1 && dir1 != 3) setPlayer1Direction(1);
         if (action == GUI.Action.down1 && dir1 != 0) setPlayer1Direction(2);
         if (action == GUI.Action.left1 && dir1 != 1) setPlayer1Direction(3);
-        if (action == GUI.Action.jump1){mov1=1;}
+        if (action == GUI.Action.jump1){if (coolDown1==0) mov1=1;}
         if (action == GUI.Action.boost1){mov1=2;}
 
         if (action == GUI.Action.up2 && dir2 != 2) setPlayer2Direction(0);
         if (action == GUI.Action.right2 && dir2 != 3) setPlayer2Direction(1);
         if (action == GUI.Action.down2 && dir2 != 0) setPlayer2Direction(2);
         if (action == GUI.Action.left2 && dir2 != 1) setPlayer2Direction(3);
-        if (action == GUI.Action.jump2){mov2=1;}
+        if (action == GUI.Action.jump2){if (coolDown2==0) mov2=1;}
         if (action == GUI.Action.boost2){mov2=2;}
 
         if (mov1 == 0) movePlayer(p1,dir1);
-        else if (mov1 == 1) jumpPlayer(p1, dir1);
+        else if (mov1 == 1){
+            jumpPlayer(p1, dir1);
+            coolDown1= 10;
+        }
         else {
             if(getModel().getBoost(p1.getNumber())==0) movePlayer(p1,dir1);
             else boostPlayer(p1, dir1);
         }
 
         if (mov2 == 0) movePlayer(p2,dir2);
-        else if (mov2 == 1) jumpPlayer(p2, dir2);
+        else if (mov2 == 1){
+            jumpPlayer(p2, dir2);
+            coolDown2= 10;
+        }
         else {
             if(getModel().getBoost(p2.getNumber())==0) movePlayer(p2,dir2);
             else boostPlayer(p2, dir2);
         }
+        getModel().setJumpCd1(coolDown1);
+        getModel().setJumpCd2(coolDown2);
     }
 }
