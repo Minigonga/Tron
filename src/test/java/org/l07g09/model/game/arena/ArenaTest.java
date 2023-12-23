91ee7b96cf7cea67dc201643189be8e7e5c06551
Manuel Mo <manuelmo.steam@gmail.com>
2023-12-23 14:31:10 +0000

    Adição de novos testes para a função collision.

--- src/test/java/org/l07g09/model/game/arena/ArenaTest.java
+++ src/test/java/org/l07g09/model/game/arena/ArenaTest.java
@@ -25,18 +25,70 @@ public class ArenaTest {
         builder = new ArenaBuilder();
         arena = builder.createArena(0,0);
         controller = new PlayersController(arena);
-        arena.setPlayer1(new Player(3,0, "#FFFFFF", '1',1));
-        arena.setPlayer2(new Player(5,0, "#000FFF", '2',3));
+        arena.setPlayer1(new Player(3,1, "#FFFFFF", '1',1));
+        arena.setPlayer2(new Player(5,1, "#000FFF", '2',3));
 
         player1 = arena.getPlayer1();
         player2 = arena.getPlayer2();
     }
     @Test
-    public void collision() {
+    public void collision1() {
         controller.movePlayer(player1, 1);
         controller.movePlayer(player2, 3);
         arena.collision();
         assertTrue(player1.getCollide());
+        assertTrue(player2.getCollide());
+    }
+
+    @Test
+    public void collision2() {
+        controller.movePlayer(player2, 3);
+        controller.movePlayer(player1, 1);
+        arena.collision();
+        assertTrue(player1.getCollide());
+        assertTrue(player2.getCollide());
+    }
+
+    @Test
+    public void collision3() {
+        controller.boostPlayer(player2, 2);
+        controller.boostPlayer(player1, 1);
+        arena.collision();
+        assertTrue(player1.getCollide());
+        assertTrue(!player2.getCollide());
+    }
+
+    @Test
+    public void collision4() {
+        controller.boostPlayer(player1, 1);
+        controller.jumpPlayer(player2, 1);
+        arena.collision();
+        assertTrue(player1.getCollide());
+        assertTrue(player2.getCollide());
+    }
+
+    @Test
+    public void collision5() {
+        arena.setPlayer1(new Player(3,1, "#FFFFFF", '1',1));
+        arena.setPlayer2(new Player(4,2, "#000FFF", '2',0));
+        player1 = arena.getPlayer1();
+        player2 = arena.getPlayer2();
+        controller.movePlayer(player1, 1);
+        controller.movePlayer(player2, 0);
+        arena.collision();
+        assertTrue(player1.getCollide());
+        assertTrue(player2.getCollide());
+    }
+
+    @Test
+    public void collision6() {
+        arena.setPlayer2(new Player(5,1, "#000FFF", '2',0));
+        player2 = arena.getPlayer2();
+        controller.jumpPlayer(player1, 1);
+        arena.collision();
+        assertTrue(!player1.getCollide());
+        controller.movePlayer(player2,0);
+        arena.collision();
         assertTrue(player2.getCollide());
     }
 
